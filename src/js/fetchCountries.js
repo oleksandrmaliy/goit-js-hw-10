9abacce05b1d06f72dd21a0f70154b582ad783c2
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-05-16 00:57:15 +0300

    done !

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,7 @@
 export { fetchCountries };
 
 function fetchCountries(name) {
-  console.log(name);
-  return fetch(
+   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(data => {
     return data.json();
